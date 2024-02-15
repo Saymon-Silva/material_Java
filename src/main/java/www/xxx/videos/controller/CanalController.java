38b6db5031030777a1c377186d5f67f89b96ac62
Saymon Patrick da Silva <saymonps@weg.net>
2024-02-14 21:08:48 -0300

    precisa da implementação de arquivo, uma controller q aceite a criacao de uma dto do usuario com/sem arquivo

--- src/main/java/www/xxx/videos/controller/CanalController.java
+++ src/main/java/www/xxx/videos/controller/CanalController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 import www.xxx.videos.model.Canal;
 import www.xxx.videos.service.CanalService;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/canal")
 @AllArgsConstructor
@@ -17,19 +19,34 @@ public class CanalController {
     public ResponseEntity<Canal> create(@RequestBody Canal canal) {
         return ResponseEntity.ok(canalService.create(canal));
     }
+
     @PutMapping
-    public ResponseEntity<Canal> update(@RequestBody Canal canal){
+    public ResponseEntity<Canal> update(@RequestBody Canal canal) {
         return ResponseEntity.ok(canalService.update(canal));
     }
+
     @DeleteMapping
-    public void delete(@RequestBody Canal canal){
+    public void delete(@RequestBody Canal canal) {
         canalService.delete(canal);
     }
+
     @DeleteMapping("/{id}")
-    public void deleteById(@PathVariable Integer id){
+    public void deleteById(@PathVariable Integer id) {
         canalService.deleteById(id);
     }
 
+    @GetMapping("/{id}")
+    public Canal findById(@PathVariable Integer id) {
+        return canalService.findById(id);
+    }
 
+    @GetMapping("/{nome}")
+    public Canal findByNome(@PathVariable String nome) {
+        return canalService.findByNome(nome);
+    }
 
+    @GetMapping
+    public List<Canal> findAll() {
+        return canalService.findAll();
+    }
 }
